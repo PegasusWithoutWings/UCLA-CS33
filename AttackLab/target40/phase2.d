b2b0837c99e7bfa1d48dfe14abad04989893626a
WinglessPegasus <georgezhangtj97@gmail.com>
2018-02-20 18:30:14 -0800

    First solution for phase2.

--- AttackLab/target40/phase2.d
+++ AttackLab/target40/phase2.d
@@ -1,10 +1,8 @@
 
-phase2.o:     file format elf64-x86-64
+phase2.o:	file format Mach-O 64-bit x86-64
 
-
-Disassembly of section .text:
-
-0000000000000000 <.text>:
-   0:	bf 6d 9f ca 55       	mov    $0x55ca9f6d,%edi
-   5:	48 83 ec 08          	sub    $0x8,%rsp
-   9:	c3                   	retq   
+Disassembly of section __TEXT,__text:
+__text:
+       0:	bf 6d 9f ca 55 	movl	$1439342445, %edi
+       5:	48 83 ec 10 	subq	$16, %rsp
+       9:	c3 	retq
